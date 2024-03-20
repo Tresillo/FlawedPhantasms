faaa46a7b59d1ac640cd4f316b8a11c077abb160
OTI-PC19PMDP\robin <robin.cowling2122@gmail.com>
2024-03-20 16:45:26 -0700

    Revert "Comments"
    This reverts commit d5f3db70635db3ae626f7fa0ed010c2457291e26.

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -18,19 +18,16 @@ signal button_deactivated()
 func _ready():
 	if not Engine.is_editor_hint():
 		_pressed = false
-		#Setup visibilities and materials
 		($BaseMesh as MeshInstance3D).layers = visibility_flags
 		($ButtonMesh as MeshInstance3D).layers = visibility_flags
 		($ButtonMesh as MeshInstance3D).mesh.material = button_material
 		
-		#Chain signals through from button activation area
 		($Activation as Area3D).body_entered.connect(func(body): manage_button(true, body))
 		($Activation as Area3D).body_exited.connect(func(body): manage_button(false, body))
 		
 		collision_layer = visibility_flags
 		set_collision_layer_value(1,true)
 		
-		#Attaches all objects that are to be controlled by this button
 		for obj in connected_objects:
 			if obj.has_method("open"):
 				button_activated.connect(obj.open)
@@ -43,7 +40,6 @@ func _ready():
 
 
 func manage_button(activate:bool, body: Node3D):
-	#cutton functionality
 	if activate and not _pressed and $Activation.has_overlapping_bodies():
 		_pressed = true
 		button_activated.emit()
