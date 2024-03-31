7e55bcd85ef54772d4c0ae7462064b9e53994c15
OTI-PC19PMDP\robin <robin.cowling2122@gmail.com>
2024-03-30 19:11:54 -0700

    Strung together levels 1, 2, and 4 using an auoloaded scene swither that switches the scene when you click on a level with a designated end level path. love the effect :)

--- Scripts/EndLevelInteractable.gd
+++ Scripts/EndLevelInteractable.gd
@@ -3,6 +3,8 @@ class_name LevelGoal
 
 @export_file("*.tscn") var next_level_path: String
 
+@onready var _level_loader: LevelLoader = get_node("/root/SceneLoaderAutoload")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,4 +12,9 @@ func _ready():
 
 func end_level():
 	print("hit end level trigger")
-	get_tree().quit()
+	
+	if next_level_path != "" and next_level_path != null:
+		_level_loader.goto_scene(next_level_path)
+		push_warning("Eventually put this into background loading")
+	else:
+		get_tree().quit()
