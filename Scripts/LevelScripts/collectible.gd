9ef04cadae9a211159876d34dc40635c05293444
Tresillo <robin.cowling2122@gmail.com>
2024-04-15 14:11:12 +0100

    fixed collectible sound not playing and collectible not being picked up

--- Scripts/LevelScripts/collectible.gd
+++ Scripts/LevelScripts/collectible.gd
@@ -8,4 +8,6 @@ func _on_body_entered(body):
 	if body is Player:
 		$CollectionSound.play()
 		pickup_collected.emit()
+		$CollectibleModel.visible = false
+		await $CollectionSound.finished
 		call_deferred("queue_free")
